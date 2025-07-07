197bccc55cd46b056c9350e38ace2b1ce7032a8b
Shashwat Singh <46977833+ShashiS32@users.noreply.github.com>
2025-07-07 12:44:03 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,29 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// src/App.tsx
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "@/contexts/AuthContext";
-import Index from "./pages/Index";
-import Practice from "./pages/Practice";
-import Videos from "./pages/Videos";
-import Resources from "./pages/Resources";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import ClassSignup from "./pages/ClassSignup";
-import ProfileView from "./pages/ProfileView";
-import ProfileSettings from "./pages/ProfileSettings";
-import NotFound from "./pages/NotFound";
+import { Header } from "@/components/Header";  // ← new!
 
-const queryClient = new QueryClient();
+import Index from "@/pages/Index";
+import Practice from "@/pages/Practice";
+import Videos from "@/pages/Videos";
+import Resources from "@/pages/Resources";
+import ClassSignup from "@/pages/ClassSignup";
+import SignUp from "@/pages/SignUp";
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/practice" element={<Practice />} />
-            <Route path="/videos" element={<Videos />} />
-            <Route path="/resources" element={<Resources />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/class-signup" element={<ClassSignup />} />
-            <Route path="/profile" element={<ProfileView />} />
-            <Route path="/profile/settings" element={<ProfileSettings />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </AuthProvider>
-  </QueryClientProvider>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      {/* ← Now every page gets this Header */}
+      <Header />
 
-export default App;
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/practice" element={<Practice />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/class-signup" element={<ClassSignup />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
